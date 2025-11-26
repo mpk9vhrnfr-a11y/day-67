935e0a4524180aaee4a0516649bca0adb251d522
sanaa <Sanaakumar07@gmail.com>
2025-11-26 17:31:59 +0530

    c

--- day 67/main.c
+++ day 67/main.c
@@ -1,15 +1,38 @@
-//
-//  main.c
-//  day 67
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
-}
+int main(void) {
+    int m, n, i, j, k;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &m);
+    int arr1[m];
+    printf("Enter elements of first array: ");
+    for(i = 0; i < m; i++)
+        scanf("%d", &arr1[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n);
+    int arr2[n];
+    printf("Enter elements of second array: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr2[i]);
+
+    int merged[m + n];
+    i = j = k = 0;
+
+    while(i < m && j < n) {
+        if(arr1[i] < arr2[j])
+            merged[k++] = arr1[i++];
+        else
+            merged[k++] = arr2[j++];
+    }
+
+    while(i < m)
+        merged[k++] = arr1[i++];
+
+    while(j < n)
+        merged[k++] = arr2[j++];
+
+    for(i = 0; i < m + n; i++) {
+        if(i != m + n - 1)
+
